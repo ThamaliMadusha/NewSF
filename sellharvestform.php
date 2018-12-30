3e17686c6ffb200361e7d058c33ad2325cd1bf3c
ThamaliMadusha <thamalimadhushasenevirathne@gmail.com>
2018-12-30 12:23:31 +0530

    Update sellharvestform.php
    adding category

--- sellharvestform.php
+++ sellharvestform.php
@@ -7,16 +7,27 @@
 	<link rel="stylesheet" type="text/css" href="css/loginstyle.css">
 </head>
 <div class="hello">	
-	<form name="inputproduct" method="POST" action="" enctype="multipart/form-data">
+	<form name="inputproduct" method="POST" action="sellharvestform.php" enctype="multipart/form-data">
 		Product Name:<br>
 		<input type="text" name="iname" ><br>
 		Product Quantity:<br>
 		<input type="number" name="quantity"><br>
 		Unit Price:<br>
         <input type="number" step="0.01" name="price"><br>
-		Upload Image:<br>
-		<input type="file" value="upload" name="fileToUpload"><br>
+        Category:<br>
+        <select name="select">
+        	<option value="vegetable">Vegetable</option>
+        	<option value="fruits"> Fruits</option>
+        	<option value="Agricultural tools">Agricultural Tools</option>
+        	<option value="Home made foods"> Home-made foods</option>
+        	<option value="Handicrafts"> Handicrafts</option>
+        	<option value="other">Other</option>
+        </select>
+		Upload Image:<br><br>
+
+		<input type="file" value="upload" name="fileToUpload"><br> <br>
 		<input type="submit" name="submit" value="Insert Product">
+		<br>
 		
 	</form>
 
@@ -28,7 +39,7 @@
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 	if(isset($_POST["submit"])){
 	move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-	$sql=mysqli_query($link,"INSERT INTO products VALUES ('','$_POST[iname]','$_POST[quantity]','$_POST[price]','$target_file')");
+	$sql=mysqli_query($link,"INSERT INTO products VALUES ('','$_POST[iname]','$_POST[select]'	,'$_POST[quantity]','$_POST[price]','$_POST[Category]' '$target_file')");
     
 }
 	
