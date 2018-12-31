af3a6150f0211faf3810fa8b5a2e695a7217f6aa
Hariniii <harinisavindra23@gmail.com>
2018-12-31 07:24:14 +0530

    Request Order Page

--- buyitems.php
+++ buyitems.php
@@ -1,3 +1,158 @@
 <?php include("includes/wateringhead.php"); ?>
 <div>
 	<h2>Purchase Food Items</h2>
+<?php  require "connection/connection.php";
+session_start();
+$pselected='Your Product Name';
+if($_SESSION["buyer"] != true){
+    ?>
+    <script type="text/Javascript">
+    window.location="login.php";
+    exit();
+
+    </script>
+    <?php
+}
+    
+    
+if(isset($_POST["submitpurchase"])){
+    mysqli_query($link,"INSERT INTO myorder VALUES('','$_POST[pname]','$_POST[sellername]','$_POST[unitprice]', '$_POST[oqty]','$_POST[fprice]', '$_SESSION[buyer]','Not Approved')");
+    $msg = "Order Purchased Success Fully";
+                echo "<script type='text/javascript'>
+                alert('$msg');
+                </script>";
+
+}   
+    ?>
+<html>
+<head>
+<title> Order</title>
+<link rel="stylesheet" type="text/css" href="css/loginstyle.css">
+</head>
+<body>
+
+<h1> Request Your Order </h1>
+<div class="hello">	
+	<form name="buyerreg" action="buyitems.php" method="POST">
+		Search You Product :<br>
+		<!-- Get product list from the datbase -->
+        <select name="pname1"  id="">
+            <?php
+      $res=mysqli_query($link,"SELECT iname from products");
+        while($row=mysqli_fetch_array($res))
+         {
+             echo"<option>";
+             echo $row["iname"];
+            echo "</option>";
+            }
+
+            ?>
+        </select>
+        <input type="submit" value="Get Available Sellers" name="submitproduct" style="width:250px;"> <br>
+		Seller Name List:<br>
+        <!-- Get available seller names -->
+        <select name="sellername" id="">
+            <?php
+            if(isset($_POST["submitproduct"])){
+                $pselected=$_POST['pname1'];
+      $res=mysqli_query($link,"SELECT sellername from products where pname= '$_POST[pname1]'");
+        while($row=mysqli_fetch_array($res))
+         {
+             echo"<option>";
+             echo $row["sellername"];
+            echo "</option>";
+            }
+
+
+        }
+        
+        ?>
+        </select>
+        Product Name:<br>
+        <input type="text" value="<?php echo $pselected; ?>" name="productname"><br>
+        <input type="submit" value="Request Product Details" name="alldetails" style="width:250px;"> <br>
+     <!-- get all the product details -->
+     <?php
+
+            if(isset($_POST["alldetails"])){
+        $res=mysqli_query($link,"SELECT * FROM products WHERE pname='$_POST[productname]'&& sellername='$_POST[sellername]'");
+        while($row=mysqli_fetch_array($res))
+         {     
+            
+             
+             $pname=$row["iname"];
+             $quantity=$row["quantity"];
+             $unitprice=$row["unitprice"];
+             $sellername=$row["sellername"]; 
+               
+
+     
+        }
+        ?>
+        Product Name:<br>
+        <input type="text" value="<?php echo $pname; ?>" name="pname" readonly><br>
+        Seller Name:<br>
+        <input type="text" value="<?php echo $sellername; ?>" name="sellername" readonly><br>
+        Unit Price:<br>
+        <input type="number" value="<?php echo $unitprice; ?>" name="unitprice" readonly><br>
+        Available Product Quantity:<br>
+        <input type="number" value="<?php echo $quantity; ?>" name="aqty" readonly><br>
+        Order Your Quantity:<br>
+        <input type="number" name="oqty"><br>
+                  
+<?php
+     }
+        ?> 
+             <input type="submit" value="Calculate my price" name="pricecal" style="width:250px;"> <br>
+
+                <?php
+         if(isset($_POST["pricecal"])){
+             if( $_POST['oqty']>$_POST['aqty']){
+                $error = "Ordered quantity Should not exceed the available quantity";
+                echo "<script type='text/javascript'>
+                alert('$error');
+                </script>";
+               
+             }elseif($_POST['oqty']==0 ||$_POST['oqty']==null){
+                $error = "Ordered quantity Should be at least 1";
+                echo "<script type='text/javascript'>
+                alert('$error');
+                </script>";
+               }else
+             {
+                $pname1= $_POST['iname'];
+                $agentname2=$_POST['sellername'];
+                $unitprice=$_POST['unitprice'];
+                $aqty=$_POST['aqty'];
+                $oqty=$_POST['oqty'];
+                $total=($_POST['unitprice']*$_POST['oqty']);
+                ?>
+                
+                 
+
+        Product Name:<br>
+        <input type="text" value=<?php echo $pname1; ?> name="iname" readonly><br>
+        Agent Name:<br>
+        <input type="text" value=<?php echo $agentname2; ?> name="aname" readonly><br>
+        Unit Price:<br>
+        <input type="number" value=<?php echo $unitprice; ?> name="unitprice" readonly><br>
+        Available Product Quantity:<br>
+        <input type="number" value="<?php echo $aqty; ?>" name="aqty" readonly><br>
+        Ordered Your Quantity:<br>
+        <input type="number" value=<?php echo $oqty;?> name="oqty"><br>
+        Final Price: <br>
+        <input type="number" name="price" value=<?php echo $finalprice; ?>><br>
+        <input type="submit" value="Purchase order" name="submitpurchase">
+                 <?php
+
+             }
+        
+         }
+        ?>
+
+		
+    </form>
+    
+</script>
+</body>
+</html>
--- sellharvestform.php
+++ sellharvestform.php
@@ -1,4 +1,7 @@
-<?php require ("connection/connection.php");?>
+<?php 
+require "connection/connection.php";
+
+ ?>
 
 <!DOCTYPE html>
 <html>
@@ -6,14 +9,26 @@
 	<title>Add Product</title>
 	<link rel="stylesheet" type="text/css" href="css/loginstyle.css">
 </head>
+<body>
 <div class="hello">	
-	<form name="inputproduct" method="POST" action="" enctype="multipart/form-data">
+	<form name="inputproduct" method="POST" action="sellharvestform.php" enctype="multipart/form-data">
 		Product Name:<br>
 		<input type="text" name="iname" ><br>
 		Product Quantity:<br>
 		<input type="number" name="quantity"><br>
 		Unit Price:<br>
         <input type="number" step="0.01" name="price"><br>
+       Category:<br>
+        <select name="cat">
+        	<option value="vegetable"> Vegetables </option>
+        	<option value="fruits"> Fruits </option>
+        	<option value="Agricultural tools"> Agricultural tools </option>
+        	<option value="Home made foods"> Home made foods </option>
+        	<option value="Handicrafts"> Handicrafts </option>
+        	<option value="other"> Other </option>
+        </select> 
+        Agent Name:<br>
+		<input type="text" name="sellername"><br>
 		Upload Image:<br>
 		<input type="file" value="upload" name="fileToUpload"><br>
 		<input type="submit" name="submit" value="Insert Product">
@@ -21,14 +36,14 @@
 	</form>
 
 	<?php
-	$target_dir = "./images";
+	if(isset($_POST["submit"])){
+	$target_dir = "Images";
 	$target_file = $target_dir.basename($_FILES["fileToUpload"]["name"]);
 	$uploadOk = 1;
 	// $username=$_SESSION["Farmer"];
 	$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-	if(isset($_POST["submit"])){
 	move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-	$sql=mysqli_query($link,"INSERT INTO products VALUES ('','$_POST[iname]','$_POST[quantity]','$_POST[price]','$target_file')");
+	$sql=mysqli_query($link,"INSERT INTO products VALUES ('','$_POST[iname]','$_POST[cat]','$_POST[quantity]','$_POST[price]',$_POST[sellername]','$target_file')");
     
 }
 	
